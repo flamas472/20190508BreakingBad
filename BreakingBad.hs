0d368793be94c2a8772da5d34a46a3933b2ac502
Federico Lamas <federicolamas95@gmail.com>
2019-04-30 09:27:45 -0300

    modelo de datos

--- BreakingBad.hs
+++ BreakingBad.hs
@@ -1,1 +1,21 @@
 module BreakingBad where
+
+    data Meta = UnaMeta {
+        color :: String,
+        peso :: Float,
+        pureza :: Int
+    }deriving (Show)
+
+    data Personaje = UnPersonaje {
+        nombre :: String,
+        estado :: String,
+        aguante :: Float,
+        purezaDeFabricacionMax :: Int,
+        meta :: Meta,
+        dinero :: Dinero
+    }deriving (Show)
+
+    data Dinero = UnDinero {
+        montoSucio :: Int,
+        montoLimpio :: Int
+    }deriving (Show)
